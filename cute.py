cc8875738842e335b08b398318836f87c3eebf6a
Alex Kersten <alex@kersten.email>
2015-02-23 18:15:25 -0600

    add database bringup and teardown

--- cute.py
+++ cute.py
@@ -1,9 +1,12 @@
 # all the imports
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
-     abort, render_template, flash
+    abort, render_template, flash
+from contextlib import closing
 
 import os
+
+VERSION = "0.0.0"
 
 # Read configuration from environment variables - these are set by the secret script that we don't commit...
 DATABASE = os.environ.get('CUTE_DB')
@@ -24,7 +27,35 @@ if SECRET_KEY is None:
     print("Secret key not set! Run through the secret shell script.")
     exit(1)
 
-print("k")
+print("Starting CuteCasa backend " + VERSION + "...")
+
+app = Flask(__name__)
+app.config.from_object(__name__)
 
 
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
 
+
+# Bringup and teardown
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
+
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
