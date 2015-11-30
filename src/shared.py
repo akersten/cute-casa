736857cd1fcba07e605e8d11ecfe6f8db923ec09
Alex Kersten <alex@kersten.email>
2015-11-29 19:04:17 -0600

    add method to get household type

--- src/shared.py
+++ src/shared.py
@@ -3,10 +3,16 @@
 #
 # Global features across the entire application.
 # ######################################################################################################################
-from flask import abort, session
 
+from flask import abort, session, g
+import queries
+from src import db
 
 def checkLogin():
     """Check that the user is logged in and transmit an HTTP error if not."""
     if not session.get('logged_in'):
         abort(401)
+
+
+def getHouseholdType(householdId):
+    return db.getSingleValue("households", "e_household_type", householdId)
