98f667a2dfdf1b9ca1a2ff2dd3bdda77827d483f
Alex Kersten <alex@kersten.email>
2015-12-25 09:12:33 -0600

    todo: tdd

--- src/tests/tests.py
+++ src/tests/tests.py
@@ -4,7 +4,7 @@ class zdbHouseholdTests(unittest.TestCase):
 
     def test_HouseholdCreatedIsHouseholdRetrieved(self):
         """A household created in the database must be able to be looked up with the returned object handle."""
-        self.assertEqual(2,4)
+        pass
 
 
 if __name__ == '__main__':
