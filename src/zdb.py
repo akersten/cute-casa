0f1518b51a131a5ab84a64e672d99e5974e94f66
Alex Kersten <alex@kersten.email>
2015-12-28 22:46:39 -0600

    convert zdb to class pattern, still need to update rest of application and separation of concerns in zdb methods

--- src/zdb.py
+++ src/zdb.py
@@ -11,73 +11,73 @@ from src import enums
 
 
 # The zdb database reference and root element..
-zdb = None
-root = None
-
-def bringup():
-    global zdb, root
-
-    """
-    Initialize the object database.
-    :return:
-    """
-    logger.logSystem("Bringing up ZODB...")
-
-    storage = ZODB.FileStorage.FileStorage('secret/cute.zdb')
-    zdb = ZODB.DB(storage)
-    connection = zdb.open()
-    root = connection.root
-
-    schemaCheckAndCreate()
-
-
-def teardown():
-    global zdb
-
-    if not zdb is None:
-        zdb.close()
-
-
-def schemaCheckAndCreate():
-    """
-    Checks the root object for the presence of the expected BTrees and creates them if they do not exist.
-    """
-    global zdb, root
-
-    if zdb is None:
-        logger.logSystem("Schema creation invoked without a database.", enums.e_log_event_level.crash)
-    if root is None:
-        logger.logSystem("Schema creation invoked without a root element.", enums.e_log_event_level.crash)
-
-
-    if not hasattr(root, 'globalSettings'):
-        logger.logSystem('Creating default global settings.')
-        #TODO
-    else:
-        logger.logSystem('Using existing global settings.')
-        #TODO
-
-
-    if not hasattr(root, 'households'):
-        logger.logSystem('Creating household collection...')
-        root.households = BTrees.OOBTree.BTree()
-    else:
-        logger.logSystem('Household collection exists, it is: ' + root.households)
-
-    transaction.commit()
+class Zdb():
 
 
 
-def getHousehold(householdId):
-    """
-    Get a household from the database.
-    :param householdId: The household id to retrieve.
-    :return: A household object corresponding to this id.
-    """
+    def __init__(self, dbPath):
+        self.zdb = None
+        self.root = None
+        self.bringup(dbPath)
 
-def createHousehold(householdId):
-    """
-    Create a household and add it to the database.
-    :param householdId: The household to create.
-    :return: A handle to the household.
-    """
+    def bringup(self, dbPath):
+        """
+        Initialize the object database.
+        :return:
+        """
+        logger.logSystem("Bringing up ZODB...")
+
+        storage = ZODB.FileStorage.FileStorage(dbPath) # usually 'secret/cute.zdb'
+        self.zdb = ZODB.DB(storage)
+        connection = zdb.open()
+        self.root = connection.root
+
+        self.schemaCheckAndCreate()
+
+
+    def teardown(self):
+        if not self.zdb is None:
+            self.zdb.close()
+
+
+    def schemaCheckAndCreate(self):
+        """
+        Checks the root object for the presence of the expected BTrees and creates them if they do not exist.
+        """
+        if self.zdb is None:
+            logger.logSystem("Schema creation invoked without a database.", enums.e_log_event_level.crash)
+        if self.root is None:
+            logger.logSystem("Schema creation invoked without a root element.", enums.e_log_event_level.crash)
+
+
+        if not hasattr(self.root, 'globalSettings'):
+            logger.logSystem('Creating default global settings.')
+            #TODO
+        else:
+            logger.logSystem('Using existing global settings.')
+            #TODO
+
+
+        if not hasattr(self.root, 'households'):
+            logger.logSystem('Creating household collection...')
+            self.root.households = BTrees.OOBTree.BTree()
+        else:
+            logger.logSystem('Household collection exists, it is: ' + self.root.households)
+
+        transaction.commit()
+
+
+
+    def getHousehold(self, householdId):
+        """
+        Get a household from the database.
+        :param householdId: The household id to retrieve.
+        :return: A household object corresponding to this id.
+        """
+
+    def createHousehold(self, householdId):
+        """
+        Create a household and add it to the database.
+        :param householdId: The household to create.
+        :return: A handle to the household.
+        """
