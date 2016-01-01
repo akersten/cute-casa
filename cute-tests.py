b6e5df4d9339046afe016a055970068d9d21e7cb
Alex Kersten <alex@kersten.email>
2016-01-01 11:55:11 -0600

    add advanced tests

--- cute-tests.py
+++ cute-tests.py
@@ -20,8 +20,32 @@ class zdbHouseholdTests(unittest.TestCase):
 
     def test_getHouseholdBasic(self):
         """Just creates a household and gets it back, a basic test."""
+        h = self.z.createHousehold("testHousehold")
+        self.assertTrue(self.z.getHousehold("testHousehold") is not None, "No household came back from the database.")
+        self.assertTrue(self.z.getHousehold("testHousehold") is h, "Household comparison failed.")
         pass
-    
+
+    def test_getHouseholdAdvanced(self):
+        """No househould should be returned for an invalid index, households should not be transposed."""
+        h1 = self.z.createHousehold("h1")
+        h2 = self.z.createHousehold("h2")
+        h3 = self.z.createHousehold("h3")
+
+        self.assertTrue(h1 is not h2)
+        self.assertTrue(h2 is not h3)
+
+        # Theoretically, the below can be replaced with "is h1" based on transitive property above, but
+        # I want to be explicit.
+        self.assertTrue(self.z.getHousehold('h1') is not h2)
+        self.assertTrue(self.z.getHousehold('h1') is not h3)
+
+        self.assertTrue(self.z.getHousehold('h1') is h1)
+        self.assertTrue(self.z.getHousehold('h2') is h2)
+        self.assertTrue(self.z.getHousehold('h3') is h3)
+
+        self.assertTrue(self.z.getHousehold('bogus') is None)
+
+
     def test_HouseholdCreatedIsHouseholdRetrieved(self):
         """A household created in the database must be able to be looked up with the returned object handle."""
 
