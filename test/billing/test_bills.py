511d5796e80c2a6a900bb39202b01658793ed0ab
Alex Kersten <alex@kersten.email>
2016-01-25 23:42:42 -0600

    add bill and billgroup test file

--- test/billing/test_bills.py
+++ test/billing/test_bills.py
@@ -1,1 +1,24 @@
 # Be sure that the values from individual contributions add up to the total calculated liability.
+
+
+import unittest
+from src.billing._bills import Bill, BillGroup
+
+class Tests_Bill(unittest.TestCase):
+    """Tests the Bill object."""
+
+    def test_init(self):
+        """
+        Bill constructor.
+        """
+        pass
+
+
+class Tests_BillGroup(unittest.TestCase):
+    """Tests the BillGroup object."""
+
+    def test_init(self):
+        """
+        Billgroup constructor.
+        """
+        pass
