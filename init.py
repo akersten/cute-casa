bbca89d9bb4b485065f7762d84d18cfe162df61d
Alex Kersten <alex@kersten.email>
2016-03-01 22:00:00 -0600

    aspirations

--- init.py
+++ init.py
@@ -2,3 +2,15 @@ from cute import init_db
 print('Regenerating CuteCasa DB...')
 init_db()
 print('Done.')
+
+
+
+yn = input('Create admin user? [y/N] ')
+if yn not in ['Y', 'y', 'yes', 'Yes']:
+    exit(0)
+
+adminName = input('Admin user name: ')
+adminPass =input('Admin user password: ')
+
+# TODO: Refactor things into routes vs. core, and have registration functionality (among other things) in core
+print('Todo...')
